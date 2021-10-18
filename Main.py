c598be9dd383a4c9d4d3d92c1db22a9957383979
Pierre Dias <cdias.pierre@gmail.com>
2021-10-18 18:03:58 -0300

    Adicionado pi.ico

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         super().setupUi(self)
-        self.setWindowIcon(QtGui.QIcon('pi.png'))
+        self.setWindowIcon(QtGui.QIcon('pi.ico'))
         self.fm = FileManager()
         self.configuration = Configuration(self.fm.get_attributes_from_config_file())
 
